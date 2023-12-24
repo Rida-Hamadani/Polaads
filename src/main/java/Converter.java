8e262a8dbcf7fa8dccfb008ea01bd13310841924
Rida-Hamadani <mridahamadani@gmail.com>
2023-12-25 01:02:21 +0200

    override toString

---
+++ src/main/java/Converter.java
@@ -0,0 +1,12 @@
+package main.java;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Converter {
+    public static HashMap<Integer, Integer> connectLists(ArrayList<Integer> domain, ArrayList<Integer> range) {
+        return new HashMap<Integer, Integer>(IntStream.range(0, domain.size())
+                .boxed()
+                .collect(Collectors.toMap(domain::get, range::get)));
+    }
+}
---
+++ src/main/java/Polynomial.java
@@ -0,0 +1,56 @@
+package main.java;
+import java.util.*;
+
+public class Polynomial {
+    private HashMap<Integer, Integer> pow_cof; // done this way to support sparse polynomials efficiently
+
+    public Polynomial(HashMap<Integer, Integer> pow_cof) {
+        this.pow_cof = pow_cof;
+    }
+
+    public Polynomial(ArrayList<Integer> powers, ArrayList<Integer> coefs) {
+        this(Converter.connectLists(powers, coefs));
+    }
+
+    private Polynomial clean() {
+        pow_cof.values().removeIf(v -> v == 0);
+        return this;
+    }
+
+    public Integer degree() {
+        return pow_cof.keySet().size() == 0
+                ? Integer.MIN_VALUE
+                : Collections.max(clean().pow_cof.keySet());
+    }
+
+    public Polynomial plus(Polynomial that) {
+        that.pow_cof.forEach((k, v) -> pow_cof.merge(k, v, (a, b) -> a + b));
+        return clean();
+    }
+
+    public Polynomial times(Integer scalar) {
+        pow_cof.forEach((k, v) -> pow_cof.put(k, scalar * v));
+        return clean();
+    }
+
+    public Polynomial times(Polynomial that) {
+        that.pow_cof.forEach((k1, v1) -> pow_cof.forEach((k2, v2) -> pow_cof.merge(k1 + k2, v1 * v2, (a, b) -> a + b)));
+        return clean();
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner sj = new StringJoiner(" + ");
+        clean().pow_cof.forEach((k, v) -> {
+            StringBuilder sb = new StringBuilder(4);
+            sb.append(v);
+            if (k != 0) {
+                sb.append('x');
+                if (k != 1) sb.append('^').append(k);
+            }
+            sj.add(sb.toString());
+        });
+        return sj.toString();
+    }
+
+}
