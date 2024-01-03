398a6d59c8dab4fd600eadd4ef03d321b79865a0
Rida-Hamadani <mridahamadani@gmail.com>
2024-01-03 21:19:25 +0200

    add basic primality test

--- src/main/java/com/polynomials/NumberTheory.java
+++ src/main/java/com/polynomials/NumberTheory.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NumberTheory {
+    public static Boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n < 4) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6)
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+
+        return true;
+    }
+
     public static Integer gcd(ArrayList<Integer> nums) {
         AtomicInteger n = new AtomicInteger(nums.get(0));
         nums.forEach(m -> n.set(gcd(n.get(), m)));
